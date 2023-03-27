9d82281ac54ba94962e77dd068b1c474229fe1c6
zhelezovartem <Zhelezovartem@gmail.com>
2023-03-27 14:50:50 +0600

    update reporter tf module to the latest version

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -2,13 +2,13 @@ module "lambda_reporter" {
   for_each = var.reporter_apps
 
   source  = "kosli-dev/kosli-reporter/aws"
-  version = "0.0.6"
+  version = "0.0.7"
 
-  name              = "${var.app_name}-${each.key}"
-  kosli_env         = var.env
-  kosli_host        = each.value.kosli_host
-  kosli_cli_version = "v2.0.0-rc1"
-  ecs_cluster       = "app"
-  kosli_org         = "cyber-dojo"
-  tags              = module.tags.result
+  name                   = "${var.app_name}-${each.key}"
+  kosli_environment_type = "ecs"
+  kosli_host             = each.value.kosli_host
+  kosli_cli_version      = "v2.0.0-rc1"
+  kosli_command          = "kosli snapshot ecs ${var.env} -C app --org cyber-dojo"
+  ecs_cluster_name       = "app"
+  tags                   = module.tags.result
 }
