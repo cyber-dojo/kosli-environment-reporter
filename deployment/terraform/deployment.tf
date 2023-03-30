0c594081c9541acf3572c15b45b9acf3c3f8acb4
Jon Jagger <jon@jaggersoft.com>
2023-03-30 06:55:20 +0100

    Move past Kosli CLI release-candidate to v2.0.0

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -7,7 +7,7 @@ module "lambda_reporter" {
   name                   = "${var.app_name}-${each.key}"
   kosli_environment_type = "ecs"
   kosli_host             = each.value.kosli_host
-  kosli_cli_version      = "v2.0.0-rc1"
+  kosli_cli_version      = "v2.0.0"
   kosli_command          = "kosli snapshot ecs ${var.env} -C app --org cyber-dojo"
   ecs_cluster_name       = "app"
   tags                   = module.tags.result
