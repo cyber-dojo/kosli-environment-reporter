b669cc66ec2d660f0741de8e92ffd135051cc960
zhelezovartem <Zhelezovartem@gmail.com>
2023-04-20 19:56:39 +0600

    tf fmt

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -4,12 +4,12 @@ module "lambda_reporter" {
   source  = "kosli-dev/kosli-reporter/aws"
   version = "0.1.3"
 
-  name                   = "${var.app_name}-${each.key}"
-  kosli_environment_type = "ecs"
-  kosli_host             = each.value.kosli_host
-  kosli_cli_version      = "v2.0.0"
+  name                       = "${var.app_name}-${each.key}"
+  kosli_environment_type     = "ecs"
+  kosli_host                 = each.value.kosli_host
+  kosli_cli_version          = "v2.0.0"
   kosli_environment_name     = var.env
   kosli_org                  = "cyber-dojo"
   reported_aws_resource_name = "app"
-  tags                   = module.tags.result
+  tags                       = module.tags.result
 }
