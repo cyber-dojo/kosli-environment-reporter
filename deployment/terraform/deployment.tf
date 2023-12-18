95ae2b5f7b00ebf2346f978ba15ef73d128fd306
zhelezovartem <zhelezovartem@gmail.com>
2023-12-18 16:53:17 +0600

    add trails reporter

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -13,3 +13,17 @@ module "lambda_reporter" {
   reported_aws_resource_name = "app"
   tags                       = module.tags.result
 }
+
+module "lambda_reporter_trails" {
+  source  = "kosli-dev/kosli-reporter/aws"
+  version = "0.1.3"
+
+  name                       = "kosli-trails-staging"
+  kosli_environment_type     = "ecs"
+  kosli_host                 = "https://staging.app.kosli.com"
+  kosli_cli_version          = "v2.6.18"
+  kosli_environment_name     = var.env
+  kosli_org                  = "cyber-dojo-trails"
+  reported_aws_resource_name = "app"
+  tags                       = module.tags.result
+}
