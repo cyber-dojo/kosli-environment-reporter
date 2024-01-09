4fc9f6b27f15e68cb21ce3679e70513b02850bf0
JonJagger <jon@kosli.com>
2024-01-09 10:36:24 +0000

    Update cli to 2.7.1 and report to cyber-dojo-trails org on prod

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -7,7 +7,7 @@ module "lambda_reporter" {
   name                       = "${var.app_name}-${each.key}"
   kosli_environment_type     = "ecs"
   kosli_host                 = each.value.kosli_host
-  kosli_cli_version          = "v2.0.0"
+  kosli_cli_version          = "v2.7.1"
   kosli_environment_name     = var.env
   kosli_org                  = "cyber-dojo"
   reported_aws_resource_name = "app"
@@ -21,7 +21,21 @@ module "lambda_reporter_trails" {
   name                       = "kosli-trails-staging"
   kosli_environment_type     = "ecs"
   kosli_host                 = "https://staging.app.kosli.com"
-  kosli_cli_version          = "v2.6.18"
+  kosli_cli_version          = "v2.7.1"
+  kosli_environment_name     = var.env
+  kosli_org                  = "cyber-dojo-trails"
+  reported_aws_resource_name = "app"
+  tags                       = module.tags.result
+}
+
+module "lambda_reporter_trails_prod" {
+  source  = "kosli-dev/kosli-reporter/aws"
+  version = "0.1.3"
+
+  name                       = "kosli-trails-prod"
+  kosli_environment_type     = "ecs"
+  kosli_host                 = "https://app.kosli.com"
+  kosli_cli_version          = "v2.7.1"
   kosli_environment_name     = var.env
   kosli_org                  = "cyber-dojo-trails"
   reported_aws_resource_name = "app"
