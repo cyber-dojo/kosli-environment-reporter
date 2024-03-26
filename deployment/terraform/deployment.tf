763457fb11dd60a662fbaa80eb253a4d8b322e89
zhelezovartem <zhelezovartem@gmail.com>
2024-03-26 20:10:59 +0500

    update kosli api token - use service account instead of personal token

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -4,12 +4,13 @@ module "lambda_reporter" {
   source  = "kosli-dev/kosli-reporter/aws"
   version = "0.1.3"
 
-  name                       = "${var.app_name}-${each.key}"
-  kosli_environment_type     = "ecs"
-  kosli_host                 = each.value.kosli_host
-  kosli_cli_version          = "v2.7.1"
-  kosli_environment_name     = var.env
-  kosli_org                  = "cyber-dojo"
-  reported_aws_resource_name = "app"
-  tags                       = module.tags.result
+  name                               = "${var.app_name}-${each.key}"
+  kosli_environment_type             = "ecs"
+  kosli_host                         = each.value.kosli_host
+  kosli_cli_version                  = "v2.7.1"
+  kosli_environment_name             = var.env
+  kosli_org                          = "cyber-dojo"
+  reported_aws_resource_name         = "app"
+  kosli_api_token_ssm_parameter_name = each.value.kosli_api_token_ssm_parameter_name
+  tags                               = module.tags.result
 }
--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -2,10 +2,12 @@ variable "reporter_apps" {
   type = map(any)
   default = {
     staging = {
-      kosli_host = "https://staging.app.kosli.com"
+      kosli_host                         = "https://staging.app.kosli.com"
+      kosli_api_token_ssm_parameter_name = "kosli_api_token_staging"
     }
     prod = {
-      kosli_host = "https://app.kosli.com"
+      kosli_host                         = "https://app.kosli.com"
+      kosli_api_token_ssm_parameter_name = "kosli_api_token_prod"
     }
   }
 }
