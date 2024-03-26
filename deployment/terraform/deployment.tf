bc8f5a69a192ab303906a95de4f331dd364b06bd
zhelezovartem <zhelezovartem@gmail.com>
2024-03-26 14:06:11 +0500

    remove cyber-dojo-trails reporters

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -13,31 +13,3 @@ module "lambda_reporter" {
   reported_aws_resource_name = "app"
   tags                       = module.tags.result
 }
-
-module "lambda_reporter_trails" {
-  source  = "kosli-dev/kosli-reporter/aws"
-  version = "0.1.3"
-
-  name                       = "kosli-trails-staging"
-  kosli_environment_type     = "ecs"
-  kosli_host                 = "https://staging.app.kosli.com"
-  kosli_cli_version          = "v2.7.1"
-  kosli_environment_name     = var.env
-  kosli_org                  = "cyber-dojo-trails"
-  reported_aws_resource_name = "app"
-  tags                       = module.tags.result
-}
-
-module "lambda_reporter_trails_prod" {
-  source  = "kosli-dev/kosli-reporter/aws"
-  version = "0.1.3"
-
-  name                       = "kosli-trails-prod"
-  kosli_environment_type     = "ecs"
-  kosli_host                 = "https://app.kosli.com"
-  kosli_cli_version          = "v2.7.1"
-  kosli_environment_name     = var.env
-  kosli_org                  = "cyber-dojo-trails"
-  reported_aws_resource_name = "app"
-  tags                       = module.tags.result
-}
