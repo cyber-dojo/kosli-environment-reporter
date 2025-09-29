997dcb00dc3125f21b7cbf67b544406df33011ad
Graham Savage <graham@kosli.com>
2025-09-29 13:42:14 +0100

    Deploy using latest Kosli Reporter module

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -1,14 +1,18 @@
 module "lambda_reporter" {
   source  = "kosli-dev/kosli-reporter/aws"
-  version = "0.5.5"
+  version = "0.8.2"
 
-  name                       = "${var.app_name}-reporter"
-  kosli_environment_type     = "ecs"
-  kosli_cli_version          = "v2.11.23"
-  kosli_environment_name     = var.env
-  kosli_org                  = "cyber-dojo"
-  kosli_host                 = var.KOSLI_HOST
-  reported_aws_resource_name = "app" # ECS cluster name
-  schedule_expression        = "rate(1 minute)"
-  tags                       = module.tags.result
+  name                = "${var.app_name}-reporter"
+  kosli_cli_version   = "v2.11.23"
+  kosli_org           = "cyber-dojo"
+  kosli_host          = var.KOSLI_HOST
+  schedule_expression = "rate(1 minute)"
+  tags                = module.tags.result
+  environments = [
+    {
+      kosli_environment_name     = var.env
+      kosli_environment_type     = "ecs"
+      reported_aws_resource_name = "app" # ECS cluster name
+    }
+  ]
 }
